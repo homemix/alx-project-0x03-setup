c02db6dbf4623e2b87e1d0b0646a7f1683d29ceb
homemix <kenmutati@gmail.com>
2025-11-20 16:23:40 +0300

    EDIT: solve interfaces common page.

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import {LayoutProps} from "@/interface";
+import { LayoutProps } from "@/interface";
 
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
