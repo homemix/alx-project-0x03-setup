8c5bb9403fff662ace1f6d481914aa17c5405c2f
homemix <kenmutati@gmail.com>
2025-11-20 16:19:24 +0300

    ADD:Resolve a 404 page not found.

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import {LayoutProps} from "@/interfaces";
+import {LayoutProps} from "@/interface";
 
 
 
